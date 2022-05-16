6f9f7f5f2a288f4c7dc88bbf6a94231fe3789962
Kofi <ebensakyi@gmail.com>
2022-05-16 16:57:57 +0000

    Menus

---
+++ pages/admin/dashboard.js
@@ -0,0 +1,46 @@
+
+import Header from '../../components/Header'
+//import Footer from '../../components/Footer'
+//import { SERVER_BASE_URL } from "../../config";
+import Dashboard from '../../components/admin/Dashboard';
+
+export default function dashboard({ examTypes, paymentTypes }) {
+    return (
+        <div >
+
+            <Header />
+
+            <Dashboard />
+
+            <Footer />
+        </div>
+    )
+}
+
+
+// export async function getServerSideProps(context) {
+//     const { token } = context.req.cookies;
+
+//     if (!token) {
+//         return {
+//             redirect: {
+//                 destination: '/auth/login',
+//                 permanent: true,
+//             },
+//         }
+//     }
+//     const examTypes = await fetch(`${SERVER_BASE_URL}/api/exam-type`).then(
+//         (res) => res.json()
+//     );
+
+//     const paymentTypes = await fetch(`${SERVER_BASE_URL}/api/payment-type`).then(
+//         (res) => res.json()
+//     );
+//     return {
+//         props: {
+//             examTypes,
+//             paymentTypes
+//         },
+//     };
+
+// }
