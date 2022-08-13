2fed21e390208e8e5bf684a8f0cc0ee222aed50d
Kofi Agyemang <ebensakyi@gmail.com>
2022-08-13 08:05:49 +0000

    u

--- prisma/seed/respondentDesignation.ts
+++ prisma/seed/respondentDesignation.ts
@@ -1,28 +1,182 @@
 export const respondentDesignation = [
-    {
-      name: "Owner",
-    },
-    {
-      name: "Manager",
-    },
-    {
-      name: "Manageress",
-    }, {
-        name: "Caretaker",
-      },
-      {
-        name: "Chief executive officer",
-      }, {
-        name: "Administrator",
-      },
-      {
-        name: "Others",
-      },
-  ];
-  
-
-  
+  {
+    name: "LandLord",
+    inspectionFormId: 1,
+  },
+  {
+    name: "LandLady",
+    inspectionFormId: 1,
+  },
+  {
+    name: "Caretaker",
+    inspectionFormId: 1,
+  },
+  {
+    name: "Tenant",
+    inspectionFormId: 1,
+  },
 
 
+  {
+    name: "Owner",
+    inspectionFormId: 2,
+  },
+  {
+    name: "Manager",
+    inspectionFormId: 2,
+  },
+  {
+    name: "Manageress",
+    inspectionFormId: 2,
+  },
+  {
+    name: "Caretaker",
+    inspectionFormId: 2,
+  },
+  {
+    name: "Chief executive officer",
+    inspectionFormId: 2,
+  },
 
 
+
+  {
+    name: "Administrator",
+    inspectionFormId: 3,
+  },
+  {
+    name: "Matron",
+    inspectionFormId: 3,
+  },
+  {
+    name: "Doctor",
+    inspectionFormId: 3,
+  },
+  {
+    name: "Nurse",
+    inspectionFormId: 3,
+  },
+
+
+  {
+    name: "Manager",
+    inspectionFormId: 4,
+  },
+  {
+    name: "Manageress",
+    inspectionFormId: 4,
+  },
+  {
+    name: "Caretaker",
+    inspectionFormId: 4,
+  }, {
+    name: "Chief Executive Officer",
+    inspectionFormId: 4,
+  }, {
+    name: "Administrator",
+    inspectionFormId: 4,
+  },
+
+
+
+  {
+    name: "Manager",
+    inspectionFormId: 5,
+  },
+  {
+    name: "Manageress",
+    inspectionFormId: 5,
+  },
+  {
+    name: "Caretaker",
+    inspectionFormId: 5,
+  }, {
+    name: "Chief Executive Officer",
+    inspectionFormId: 5,
+  }, {
+    name: "Headmaster",
+    inspectionFormId: 5,
+  },{
+    name: "Headmistress",
+    inspectionFormId: 5,
+  },{
+    name: "Proprietor",
+    inspectionFormId: 5,
+  },
+
+
+
+
+  {
+    name: "Manager",
+    inspectionFormId: 6,
+  },
+  {
+    name: "Manageress",
+    inspectionFormId: 6,
+  },
+  {
+    name: "Caretaker",
+    inspectionFormId: 6,
+  }, {
+    name: "Chief Executive Officer",
+    inspectionFormId: 6,
+  }, {
+    name: "Administrator",
+    inspectionFormId: 6,
+  },
+
+
+  {
+    name: "Manager",
+    inspectionFormId: 7,
+  },
+  {
+    name: "Manageress",
+    inspectionFormId: 7,
+  },
+  {
+    name: "Caretaker",
+    inspectionFormId: 7,
+  }, {
+    name: "Station master",
+    inspectionFormId: 7,
+  },
+  , {
+    name: "Driver",
+    inspectionFormId: 7,
+  }, {
+    name: "Trader",
+    inspectionFormId: 7,
+  }, 
+
+
+  {
+    name: "Sexton",
+    inspectionFormId: 8,
+  },
+  {
+    name: "Attendant",
+    inspectionFormId: 8,
+  },
+  {
+    name: "Manager",
+    inspectionFormId: 8,
+  },
+  {
+    name: "Manageress",
+    inspectionFormId: 8,
+  },
+  {
+    name: "Caretaker",
+    inspectionFormId: 8,
+  },
+  {
+    name: "Chief executive officer",
+    inspectionFormId: 8,
+  }, {
+    name: "Administrator",
+    inspectionFormId: 8,
+  },
+
+];
