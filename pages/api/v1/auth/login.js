ee7622a0b7d4b19241782cc9877077fc5ad26d86
Kofi Agyemang <ebensakyi@gmail.com>
2022-09-21 22:08:36 +0000

    updated

---
+++ pages/api/v1/auth/login.js
@@ -0,0 +1,44 @@
+import prisma from "../../../../prisma/MyPrismaClient";
+
+const post = async (req, res) => {
+  try {
+    const data = {
+     
+      phoneNumber: req.body.phoneNumber,
+      password: req.body.password,
+    
+    };
+    console.log(req.body);
+    // const user = await prisma.user.create({ data :data});
+    // return res
+    //   .status(200)
+    //   .json({ statusCode: 1, message: "Data saved", data: { user } });
+  } catch (error) {
+    console.log(error);
+    if (error.code === "P2002")
+      return res
+        .status(200)
+        .json({ statusCode: 0, message: "user prefix should be unique" });
+  }
+};
+
+const get = async (req, res) => {
+  try {
+    const user = await prisma.user.findMany({ where: { deleted: 0 } });
+    return res.status(200).json({ statusCode: 1, data: user });
+  } catch (error) {
+    console.log("Error: " + error);
+  }
+};
+
+export default (req, res) => {
+  req.method === "POST"
+    ? post(req, res)
+    : req.method === "PUT"
+    ? console.log("PUT")
+    : req.method === "DELETE"
+    ? console.log("DELETE")
+    : req.method === "GET"
+    ? get(req, res)
+    : res.status(404).send("");
+};
