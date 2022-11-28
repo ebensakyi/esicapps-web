d07e00cf846715b592bdf7cef457911eac594297
Kofi Agyemang <ebensakyi@gmail.com>
2022-11-28 21:03:13 +0000

    Update

--- prisma/seed/nuisance.ts
+++ prisma/seed/nuisance.ts
@@ -1,802 +1,111 @@
 export const nuisance = [
   {
     name: "Exposed Excretment",
-    inspectionFormId: 1,
   },
   {
-    name: "Insanitary bathroom",
-    inspectionFormId: 1,
+    name: "Unsanitary bathroom",
   },
   {
     name: "Growth of weeds",
-    inspectionFormId: 1,
   },
   {
     name: "Detrimental accumulation of articles",
-    inspectionFormId: 1,
   },
   {
     name: "Insanitary Latrine",
-    inspectionFormId: 1,
   },
   {
     name: "Noise",
-    inspectionFormId: 1,
   },
   {
     name: "No toilet",
-    inspectionFormId: 1,
   },
   {
     name: "Dust",
-    inspectionFormId: 1,
   },
   {
     name: "Overcrowding",
-    inspectionFormId: 1,
   },
   {
     name: "Accumulation of Refuse",
-    inspectionFormId: 1,
   },
 
   {
     name: "Prohibited animals",
-    inspectionFormId: 1,
   },
   {
     name: "Insanitary drains",
-    inspectionFormId: 1,
   },
   {
     name: "Insanitary catchpit",
-    inspectionFormId: 1,
   },
   {
     name: "Dirty walls of premises",
-    inspectionFormId: 1,
   },
   {
     name: "Rats and other vermin",
-    inspectionFormId: 1,
   },
   {
     name: "Construction waste",
-    inspectionFormId: 1,
   },
 
   {
      name: "Stagnant waste water",
-   inspectionFormId: 1
    },
   {
-    name: "Exposed food",
-    inspectionFormId: 1,
+    name: "Exposed Faecal matter",
   },
 
   {
     name: "Offensive odour",
-    inspectionFormId: 1,
   },
   {
     name: "Insanitary Animal Space",
-    inspectionFormId: 1,
   },
 
   {
     name: "Unsafe structure",
-    inspectionFormId: 1,
+  },
+  {
+    name: "Stray animals",
   },
   {
     name: "Dumping and Burning",
-    inspectionFormId: 1,
   },
+  {name: "Insanitary bathroom"},
+  {name: "Expired Medicine"},
+  {name: "Exposed food"},
+
   {
     name: "No Permit",
-    inspectionFormId: 1,
+  },
+  {name: "Insanitary kitchen"},
+
+  {
+    name: "Defective Placenta Pit",
   },
   {
-    name: "No nuisance",
-    inspectionFormId: 1,
-  },
-  {
-    name: "Exposed Excretment",
-    inspectionFormId: 2,
-  },
-  {
-    name: "Insanitary  bathroom",
-    inspectionFormId: 2,
-  },
-  {
-    name: "Growth of weeds",
-    inspectionFormId: 2,
-  },
-  {
-    name: "Detrimental accumulation of articles",
-    inspectionFormId: 2,
-  },
-  {
-    name: "Insanitary Latrine",
-    inspectionFormId: 2,
-  },
-  {
-    name: "Noise",
-    inspectionFormId: 2,
-  },
-  {
-    name: "No toilet",
-    inspectionFormId: 2,
-  },
-  {
-    name: "Dust",
-    inspectionFormId: 2,
-  },
-  {
-    name: "Overcrowding",
-    inspectionFormId: 2,
+    name: "Lack Of Placenta Pit",
   },
   {
     name: "Accumulation of Refuse",
-    inspectionFormId: 2,
   },
   {
-    name: "Expired drugs",
-    inspectionFormId: 2,
+    name: "Defective Containment",
   },
   {
-    name: "Prohibited animals",
-    inspectionFormId: 2,
+    name: "Broken Pipe",
   },
   {
-    name: "Insanitary drains",
-    inspectionFormId: 2,
+    name: "Defective Tomb",
   },
   {
-    name: "Insanitary catchpit",
-    inspectionFormId: 2,
+    name: "Improper Tomb Layout",
   },
   {
-    name: "Dirty walls of premises",
-    inspectionFormId: 2,
+    name: "Exposed Anal Cleansing Material",
   },
-  {
-    name: "Rats and other vermin",
-    inspectionFormId: 2,
-  },
-  {
-    name: "Construction waste",
-    inspectionFormId: 2,
-  },
-
-  { name: "Stagnant waste water", inspectionFormId: 2 },
-  {
-    name: "Exposed food",
-    inspectionFormId: 2,
-  },
-
-  {
-    name: "Offensive odour",
-    inspectionFormId: 2,
-  },
-  {
-    name: "Insanitary Animal Space",
-    inspectionFormId: 2,
-  },
-
-  {
-    name: "Unsafe structure",
-    inspectionFormId: 2,
-  },
-  {
-    name: "Dumping and Burning",
-    inspectionFormId: 2,
-  },
-  {
-    name: "No Permit",
-    inspectionFormId: 2,
-  },
-  {
-    name: "No nuisance",
-    inspectionFormId: 2,
-  },
-  {
-    name: "Exposed Excretment",
-    inspectionFormId: 3,
-  },
-  {
-    name: "Insanitary  bathroom",
-    inspectionFormId: 3,
-  },
-  {
-    name: "Growth of weeds",
-    inspectionFormId: 3,
-  },
-  {
-    name: "Detrimental accumulation of articles",
-    inspectionFormId: 3,
-  },
-  {
-    name: "Insanitary Latrine",
-    inspectionFormId: 3,
-  },
-  {
-    name: "Noise",
-    inspectionFormId: 3,
-  },
-  {
-    name: "No toilet",
-    inspectionFormId: 3,
-  },
-  {
-    name: "Dust",
-    inspectionFormId: 3,
-  },
-  {
-    name: "Overcrowding",
-    inspectionFormId: 3,
-  },
-  {
-    name: "Accumulation of Refuse",
-    inspectionFormId: 3,
-  },
-  {
-    name: "Expired drugs",
-    inspectionFormId: 3,
-  },
-  {
-    name: "Prohibited animals",
-    inspectionFormId: 3,
-  },
-  {
-    name: "Insanitary drains",
-    inspectionFormId: 3,
-  },
-  {
-    name: "Insanitary catchpit",
-    inspectionFormId: 3,
-  },
-  {
-    name: "Dirty walls of premises",
-    inspectionFormId: 3,
-  },
-  {
-    name: "Rats and other vermin",
-    inspectionFormId: 3,
-  },
-  {
-    name: "Construction waste",
-    inspectionFormId: 3,
-  },
-
-  { name: "Stagnant waste water", inspectionFormId: 3 },
-  {
-    name: "Exposed food",
-    inspectionFormId: 3,
-  },
-
-  {
-    name: "Offensive odour",
-    inspectionFormId: 3,
-  },
-  {
-    name: "Insanitary Animal Space",
-    inspectionFormId: 3,
-  },
-
-  {
-    name: "Unsafe structure",
-    inspectionFormId: 3,
-  },
-  {
-    name: "Dumping and Burning",
-    inspectionFormId: 3,
-  },
-  {
-    name: "No Permit",
-    inspectionFormId: 3,
-  },
-  {
-    name: "No nuisance",
-    inspectionFormId: 3,
-  },
-  {
-    name: "Exposed Excretment",
-    inspectionFormId: 4,
-  },
-  {
-    name: "Insanitary  bathroom",
-    inspectionFormId: 4,
-  },
-  {
-    name: "Growth of weeds",
-    inspectionFormId: 4,
-  },
-  {
-    name: "Detrimental accumulation of articles",
-    inspectionFormId: 4,
-  },
-  {
-    name: "Insanitary Latrine",
-    inspectionFormId: 4,
-  },
-  {
-    name: "Noise",
-    inspectionFormId: 4,
-  },
-  {
-    name: "No toilet",
-    inspectionFormId: 4,
-  },
-  {
-    name: "Dust",
-    inspectionFormId: 4,
-  },
-  {
-    name: "Overcrowding",
-    inspectionFormId: 4,
-  },
-  {
-    name: "Accumulation of Refuse",
-    inspectionFormId: 4,
-  },
-  {
-    name: "Expired drugs",
-    inspectionFormId: 4,
-  },
-  {
-    name: "Prohibited animals",
-    inspectionFormId: 4,
-  },
-  {
-    name: "Insanitary drains",
-    inspectionFormId: 4,
-  },
-  {
-    name: "Insanitary catchpit",
-    inspectionFormId: 4,
-  },
-  {
-    name: "Dirty walls of premises",
-    inspectionFormId: 4,
-  },
-  {
-    name: "Rats and other vermin",
-    inspectionFormId: 4,
-  },
-  {
-    name: "Construction waste",
-    inspectionFormId: 4,
-  },
-
-  { name: "Stagnant waste water", inspectionFormId: 4 },
-  {
-    name: "Exposed food",
-    inspectionFormId: 4,
-  },
-
-  {
-    name: "Offensive odour",
-    inspectionFormId: 4,
-  },
-  {
-    name: "Insanitary Animal Space",
-    inspectionFormId: 4,
-  },
-
-  {
-    name: "Unsafe structure",
-    inspectionFormId: 4,
-  },
-  {
-    name: "Dumping and Burning",
-    inspectionFormId: 4,
-  },
-  {
-    name: "No Permit",
-    inspectionFormId: 4,
-  },
-  {
-    name: "No nuisance",
-    inspectionFormId: 4,
-  },
-  {
-    name: "Exposed Excretment",
-    inspectionFormId: 5,
-  },
-  {
-    name: "Insanitary  bathroom",
-    inspectionFormId: 5,
-  },
-  {
-    name: "Growth of weeds",
-    inspectionFormId: 5,
-  },
-  {
-    name: "Detrimental accumulation of articles",
-    inspectionFormId: 5,
-  },
-  {
-    name: "Insanitary Latrine",
-    inspectionFormId: 5,
-  },
-  {
-    name: "Noise",
-    inspectionFormId: 5,
-  },
-  {
-    name: "No toilet",
-    inspectionFormId: 5,
-  },
-  {
-    name: "Dust",
-    inspectionFormId: 5,
-  },
-  {
-    name: "Overcrowding",
-    inspectionFormId: 5,
-  },
-  {
-    name: "Accumulation of Refuse",
-    inspectionFormId: 5,
-  },
-  {
-    name: "Expired drugs",
-    inspectionFormId: 5,
-  },
-  {
-    name: "Prohibited animals",
-    inspectionFormId: 5,
-  },
-  {
-    name: "Insanitary drains",
-    inspectionFormId: 5,
-  },
-  {
-    name: "Insanitary catchpit",
-    inspectionFormId: 5,
-  },
-  {
-    name: "Dirty walls of premises",
-    inspectionFormId: 5,
-  },
-  {
-    name: "Rats and other vermin",
-    inspectionFormId: 5,
-  },
-  {
-    name: "Construction waste",
-    inspectionFormId: 5,
-  },
-
-  { name: "Stagnant waste water", inspectionFormId: 5 },
-  {
-    name: "Exposed food",
-    inspectionFormId: 5,
-  },
-
-  {
-    name: "Offensive odour",
-    inspectionFormId: 5,
-  },
-  {
-    name: "Insanitary Animal Space",
-    inspectionFormId: 5,
-  },
-
-  {
-    name: "Unsafe structure",
-    inspectionFormId: 5,
-  },
-  {
-    name: "Dumping and Burning",
-    inspectionFormId: 5,
-  },
-  {
-    name: "No Permit",
-    inspectionFormId: 5,
-  },
-  {
-    name: "No nuisance",
-    inspectionFormId: 5,
-  },
-  {
-    name: "Exposed Excretment",
-    inspectionFormId: 6,
-  },
-  {
-    name: "Insanitary  bathroom",
-    inspectionFormId: 6,
-  },
-  {
-    name: "Growth of weeds",
-    inspectionFormId: 6,
-  },
-  {
-    name: "Detrimental accumulation of articles",
-    inspectionFormId: 6,
-  },
-  {
-    name: "Insanitary Latrine",
-    inspectionFormId: 6,
-  },
-  {
-    name: "Noise",
-    inspectionFormId: 6,
-  },
-  {
-    name: "No toilet",
-    inspectionFormId: 6,
-  },
-  {
-    name: "Dust",
-    inspectionFormId: 6,
-  },
-  {
-    name: "Overcrowding",
-    inspectionFormId: 6,
-  },
-  {
-    name: "Accumulation of Refuse",
-    inspectionFormId: 6,
-  },
-  {
-    name: "Expired drugs",
-    inspectionFormId: 6,
-  },
-  {
-    name: "Prohibited animals",
-    inspectionFormId: 6,
-  },
-  {
-    name: "Insanitary drains",
-    inspectionFormId: 6,
-  },
-  {
-    name: "Insanitary catchpit",
-    inspectionFormId: 6,
-  },
-  {
-    name: "Dirty walls of premises",
-    inspectionFormId: 6,
-  },
-  {
-    name: "Rats and other vermin",
-    inspectionFormId: 6,
-  },
-  {
-    name: "Construction waste",
-    inspectionFormId: 6,
-  },
-
-  { name: "Stagnant waste water", inspectionFormId: 6 },
-  {
-    name: "Exposed food",
-    inspectionFormId: 6,
-  },
-
-  {
-    name: "Offensive odour",
-    inspectionFormId: 6,
-  },
-  {
-    name: "Insanitary Animal Space",
-    inspectionFormId: 6,
-  },
-
-  {
-    name: "Unsafe structure",
-    inspectionFormId: 6,
-  },
-  {
-    name: "Dumping and Burning",
-    inspectionFormId: 6,
-  },
-  {
-    name: "No Permit",
-    inspectionFormId: 6,
-  },
-  {
-    name: "No nuisance",
-    inspectionFormId: 6,
-  },
-  {
-    name: "Exposed Excretment",
-    inspectionFormId: 7,
-  },
-  {
-    name: "Insanitary  bathroom",
-    inspectionFormId: 7,
-  },
-  {
-    name: "Growth of weeds",
-    inspectionFormId: 7,
-  },
-  {
-    name: "Detrimental accumulation of articles",
-    inspectionFormId: 7,
-  },
-  {
-    name: "Insanitary Latrine",
-    inspectionFormId: 7,
-  },
-  {
-    name: "Noise",
-    inspectionFormId: 7,
-  },
-  {
-    name: "No toilet",
-    inspectionFormId: 7,
-  },
-  {
-    name: "Dust",
-    inspectionFormId: 7,
-  },
-  {
-    name: "Overcrowding",
-    inspectionFormId: 7,
-  },
-  {
-    name: "Accumulation of Refuse",
-    inspectionFormId: 7,
-  },
-  {
-    name: "Expired drugs",
-    inspectionFormId: 7,
-  },
-  {
-    name: "Prohibited animals",
-    inspectionFormId: 7,
-  },
-  {
-    name: "Insanitary drains",
-    inspectionFormId: 7,
-  },
-  {
-    name: "Insanitary catchpit",
-    inspectionFormId: 7,
-  },
-  {
-    name: "Dirty walls of premises",
-    inspectionFormId: 7,
-  },
-  {
-    name: "Rats and other vermin",
-    inspectionFormId: 7,
-  },
-  {
-    name: "Construction waste",
-    inspectionFormId: 7,
-  },
-
-  { name: "Stagnant waste water", inspectionFormId: 7 },
-  {
-    name: "Exposed food",
-    inspectionFormId: 7,
-  },
-
-  {
-    name: "Offensive odour",
-    inspectionFormId: 7,
-  },
-  {
-    name: "Insanitary Animal Space",
-    inspectionFormId: 7,
-  },
-
-  {
-    name: "Unsafe structure",
-    inspectionFormId: 7,
-  },
-  {
-    name: "Dumping and Burning",
-    inspectionFormId: 7,
-  },
-  {
-    name: "No Permit",
-    inspectionFormId: 7,
-  },
-  {
-    name: "No nuisance",
-    inspectionFormId: 7,
-  },
-  {
-    name: "Exposed Excretment",
-    inspectionFormId: 8,
-  },
-  {
-    name: "Insanitary  bathroom",
-    inspectionFormId: 8,
-  },
-  {
-    name: "Growth of weeds",
-    inspectionFormId: 8,
-  },
-  {
-    name: "Detrimental accumulation of articles",
-    inspectionFormId: 8,
-  },
-  {
-    name: "Insanitary Latrine",
-    inspectionFormId: 8,
-  },
-  {
-    name: "Noise",
-    inspectionFormId: 8,
-  },
-  {
-    name: "No toilet",
-    inspectionFormId: 8,
-  },
-  {
-    name: "Dust",
-    inspectionFormId: 8,
-  },
-  {
-    name: "Overcrowding",
-    inspectionFormId: 8,
-  },
-  {
-    name: "Accumulation of Refuse",
-    inspectionFormId: 8,
-  },
-  {
-    name: "Expired drugs",
-    inspectionFormId: 8,
-  },
-  {
-    name: "Prohibited animals",
-    inspectionFormId: 8,
-  },
-  {
-    name: "Insanitary drains",
-    inspectionFormId: 8,
-  },
-  {
-    name: "Insanitary catchpit",
-    inspectionFormId: 8,
-  },
-  {
-    name: "Dirty walls of premises",
-    inspectionFormId: 8,
-  },
-  {
-    name: "Rats and other vermin",
-    inspectionFormId: 8,
-  },
-  {
-    name: "Construction waste",
-    inspectionFormId: 8,
-  },
-
-  { name: "Stagnant waste water", inspectionFormId: 8 },
-  {
-    name: "Exposed food",
-    inspectionFormId: 8,
-  },
-
-  {
-    name: "Offensive odour",
-    inspectionFormId: 8,
-  },
-  {
-    name: "Insanitary Animal Space",
-    inspectionFormId: 8,
-  },
-
-  {
-    name: "Unsafe structure",
-    inspectionFormId: 8,
-  },
-  {
-    name: "Dumping and Burning",
-    inspectionFormId: 8,
-  },
-  {
-    name: "No Permit",
-    inspectionFormId: 8,
-  },
-  {
-    name: "No nuisance",
-    inspectionFormId: 8,
-  },
+ 
 ];
+
+
