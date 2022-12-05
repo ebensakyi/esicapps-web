ff47b2de9312e2b0170a580bac0b5098fd546b0d
Kofi Agyemang <ebensakyi@gmail.com>
2022-12-05 16:42:05 +0000

    Update

--- README.md
+++ README.md
@@ -2,3 +2,11 @@
 2. Add user logs to all tables
 
 
+cd ~/
+git clone https://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@github.com/ebensakyi/esicapps-web.git
+cd ~/esicapps-web
+sudo mv * /var/www/html/
+prisma generate
+sudo npm build
+pm2 delete esicapps
+pm2 start npm --name esicapps -- run start
---
+++ pages/api/v1/sections/basic-info-section.js
@@ -0,0 +1,55 @@
+import prisma from "../../../../prisma/MyPrismaClient";
+
+const post = async (req, res) => {
+  try {
+    const data = {
+      inspectionId: req.body.inspectionId,
+      userId: Number(req.body.userId),
+      communityId: Number(req.body.communityId),
+      community: req.body.community == "null" ? "" : req.body.community,
+      ghanaPostGps: req.body.ghanaPostGps,
+      latitude: req.body.latitude,
+      longitude: req.body.longitude,
+      accuracy: req.body.accuracy,
+      respondentName: req.body.respondentName,
+      respondentPhoneNumber: req.body.respondentPhoneNumber,
+      respondentDesignationId: Number(req.body.respondentDesignationId),
+    };
+
+   // console.log(req.body.inspectionId);
+    console.log(data);
+
+    const response = await prisma.basicInfoSection.create({ data });
+    res.status(200).json({ statusCode: 1, message: "Data saved" });
+  } catch (error) {
+    console.log("Error: " + error);
+    if (error.code === "P2002")
+      return res
+        .status(400)
+        .json({ statusCode: 0, message: "dataVersion s should be unique" });
+  }
+};
+
+const get = async (req, res) => {
+  try {
+    const dataVersion = await prisma.dataVersion.findMany({
+      where: { deleted: 0 },
+    });
+    //return res.status(200).json({ statusCode: 1, data: dataVersion });
+    return res.status(200).json(dataVersion);
+  } catch (error) {
+    console.log("Error: " + error);
+  }
+};
+
+export default (req, res) => {
+  req.method === "POST"
+    ? post(req, res)
+    : req.method === "PUT"
+    ? console.log("PUT")
+    : req.method === "DELETE"
+    ? console.log("DELETE")
+    : req.method === "GET"
+    ? get(req, res)
+    : res.status(404).send("");
+};
