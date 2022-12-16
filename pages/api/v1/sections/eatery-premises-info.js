0e7ac5df83208fb733e8c4a02a8627d8d2aa80af
Kofi Agyemang <ebensakyi@gmail.com>
2022-12-16 22:21:53 +0000

    Update

--- pages/api/v1/sections/eatery-premises-info.js
+++ pages/api/v1/sections/eatery-premises-info.js
@@ -5,64 +5,105 @@ const post = async (req, res) => {
   try {
     console.log(req.body);
 
-    // const data = {
-    //   id: req.body.id,
-    //   inspectionId: req.body.inspectionId,
-    //   userId: Number(req.body.userId),
-    //   drainsAvailabilityId:
-    //     req.body.drainsAvailabilityId == "null"
-    //       ? null
-    //       : Number(req.body.drainsAvailabilityId),
-    //   toiletAvailabilityId:
-    //     req.body.toiletAvailabilityId == "null"
-    //       ? ""
-    //       : Number(req.body.toiletAvailabilityId),
-    //   urinalAvailabilityId:
-    //     req.body.urinalAvailabilityId == "null"
-    //       ? null
-    //       : Number(req.body.urinalAvailabilityId),
+    const data = {
+      id: req.body.id,
+      inspectionId: req.body.inspectionId,
+      userId: Number(req.body.userId),
+      facilityName: req.body.facilityName,
+      physicalStructureTypeId:
+        req.body.physicalStructureTypeId == "null"
+          ? null
+          : Number(req.body.physicalStructureTypeId),
+      eateryPremisesTypeId:
+        req.body.eateryPremisesTypeId == "null"
+          ? null
+          : Number(req.body.eateryPremisesTypeId),
+      eateryPremisesSubTypeId:
+        req.body.eateryPremisesSubTypeId == "null"
+          ? null
+          : Number(req.body.eateryPremisesSubTypeId),
+      designatedSmokingAreaId:
+        req.body.designatedSmokingAreaId == "null"
+          ? null
+          : Number(req.body.designatedSmokingAreaId),
 
-    //   bathRoomAvailabilityId:
-    //     req.body.bathRoomAvailabilityId == "null"
-    //       ? null
-    //       : Number(req.body.bathRoomAvailabilityId),
-    //   approvedHandwashingFacilityAvailabilityId:
-    //     req.body.approvedHandwashingFacilityAvailabilityId == "null"
-    //       ? null
-    //       : Number(req.body.approvedHandwashingFacilityAvailabilityId),
-    //   householdNumber:
-    //     req.body.householdNumber ,
-    //   maleOccupantNumber:
-    //     req.body.maleOccupantNumber == "null"
-    //       ? null
-    //       : Number(req.body.maleOccupantNumber),
-    //   femaleOccupantNumber:
-    //     req.body.femaleOccupantNumber == "null"
-    //       ? null
-    //       : Number(req.body.femaleOccupantNumber),
+      drainsAvailabilityId:
+        req.body.drainsAvailabilityId == "null"
+          ? null
+          : Number(req.body.drainsAvailabilityId),
+      toiletAvailabilityId:
+        req.body.toiletAvailabilityId == "null"
+          ? ""
+          : Number(req.body.toiletAvailabilityId),
+      urinalAvailabilityId:
+        req.body.urinalAvailabilityId == "null"
+          ? null
+          : Number(req.body.urinalAvailabilityId),
 
-    //   animalNumber:
-    //     req.body.animalNumber == "null" ? null : Number(req.body.animalNumber),
+      bathRoomAvailabilityId:
+        req.body.bathRoomAvailabilityId == "null"
+          ? null
+          : Number(req.body.bathRoomAvailabilityId),
 
-    //   animalAvailabilityId:
-    //     req.body.animalAvailabilityId == "null"
-    //       ? null
-    //       : Number(req.body.animalAvailabilityId),
+      approvedHandwashingFacilityAvailabilityId:
+        req.body.approvedHandwashingFacilityAvailabilityId == "null"
+          ? null
+          : Number(req.body.approvedHandwashingFacilityAvailabilityId),
 
-    //   otherAnimal: req.body.otherAnimal,
-    //   animalSpaceConditionId:
-    //     req.body.animalSpaceConditionId == "null"
-    //       ? null
-    //       : Number(req.body.animalSpaceConditionId),
+      firstAidAvailabilityId:
+        req.body.firstAidAvailabilityId == "null"
+          ? null
+          : Number(req.body.firstAidAvailabilityId),
+      kitchenAvailabilityId:
+        req.body.kitchenAvailabilityId == "null"
+          ? null
+          : Number(req.body.kitchenAvailabilityId),
 
-    //   vaccinationProofId:
-    //     req.body.vaccinationProofId == "null"
-    //       ? null
-    //       : Number(req.body.vaccinationProofId),
-    // };
+      protectiveClothingId:
+        req.body.protectiveClothingId == "null"
+          ? null
+          : Number(req.body.protectiveClothingId),
+
+      numberFoodHandling:
+        req.body.numberFoodHandling == "null"
+          ? null
+          : Number(req.body.numberFoodHandling),
+
+      numberFoodHandlingMedical:
+        req.body.numberFoodHandlingMedical == "null"
+          ? null
+          : Number(req.body.numberFoodHandlingMedical),
+
+      uncookedFoodStorageCondtionSafeId:
+        req.body.uncookedFoodStorageCondtionSafeId == "null"
+          ? null
+          : Number(req.body.uncookedFoodStorageCondtionSafeId),
+
+      cookedFoodStorageCondtionSafeId:
+        req.body.cookedFoodStorageCondtionSafeId == "null"
+          ? null
+          : Number(req.body.cookedFoodStorageCondtionSafeId),
+      disinfestationId:
+        req.body.disinfestationId == "null"
+          ? null
+          : Number(req.body.disinfestationId),
+      disinfestationFrequencyId:
+        req.body.disinfestationFrequencyId == "null"
+          ? null
+          : Number(req.body.disinfestationFrequencyId),
+      disinfectionId:
+        req.body.disinfectionId == "null"
+          ? null
+          : Number(req.body.disinfectionId),
+
+      disinfectionFrequencyId:
+        req.body.disinfectionFrequencyId == "null"
+          ? null
+          : Number(req.body.disinfectionFrequencyId),
+    };
 
     // console.log(data);
-    // const response = await prisma.residentialPremisesInfoSection.create({ data });
+   const response = await prisma.eateryPremisesInfoSection.create({ data });
 
     res.status(200).json({ statusCode: 1, message: "Data saved" });
   } catch (error) {
