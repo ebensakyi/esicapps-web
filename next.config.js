455f2088651faf0a9cd7129545a4a65798c2766d
Kofi Agyemang <ebensakyi@gmail.com>
2023-01-14 22:06:12 +0000

    Update

--- next.config.js
+++ next.config.js
@@ -1,12 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  "rules": {
-    "@next/next/no-sync-scripts": "off"
-  }, images: {
+  rules: {
+    "@next/next/no-sync-scripts": "off",
+  },
+  images: {
     domains: ["sanitation-reporter-images.s3.eu-west-2.amazonaws.com"],
   },
+};
 
-}
-
-module.exports = nextConfig
+module.exports = nextConfig;
