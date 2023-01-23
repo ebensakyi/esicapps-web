5ec94b44e42c85b3dabd670f1798aa7f5265779f
Kofi Agyemang <ebensakyi@gmail.com>
2023-01-23 09:57:32 +0000

    Update

---
+++ prisma/seed/userGuides.ts
@@ -0,0 +1,24 @@
+export const userGuides = [
+  {
+    title: "title",
+    fileType: "mp4",
+    action: "download",
+    link: "youtube.com",
+    description: "Please make sure your database server is running",
+  },
+  {
+    title: "",
+    fileType: "",
+    action: "",
+    link: "",
+    description: "",
+  },
+  {
+    title: "",
+    fileType: "",
+    action: "",
+    link: "",
+    description: "",
+  },
+ 
+];
