6392baf554479f8d8da96ba6c34f8f2846512f95
Kofi Agyemang <ebensakyi@gmail.com>
2023-03-15 20:33:26 +0000

    Update

--- pages/api/v1/messaging/sms/broadcast.js
+++ pages/api/v1/messaging/sms/broadcast.js
@@ -4,18 +4,19 @@ import { append_233 } from "../../../../../helpers/append-233";
 import { getUserCookie } from "../../../../../helpers/cookies-manager";
 
 const post = async (req, res) => {
-  // try {
+   try {
 
   let userCookie = await getUserCookie(req, res);
 
   let recipientId = req.body.recipient.split("$")[0];
   let recipient = req.body.recipient.split("$")[1];
+  let group = Number(req.body.group);
 
   const data = {
     recipient: recipient,
     message: req.body.message,
     title: req.body.title,
-    recipientTag: Number(req.body.group),
+    recipientTag: group,
     recipientId: Number(recipientId),
     sender: Number(userCookie.user.id),
     messageType: 2,
@@ -23,42 +24,37 @@ const post = async (req, res) => {
   };
 
   const response = await prisma.messaging.create({ data });
-  // if (recipient != null || recipient != "") {
-  //   const res = await prisma.user.findMany({
-  //     where: { deleted: 0, id: recipient },
-  //   });
+  if (group == 1) {
+    //Send broadcast sms to district
+    const res = await prisma.user.findMany({
+      where: { deleted: 0, districtId: Number(recipientId) },
+    });
 
-  //   for (let i = 0; i < res.length; i++) {
-  //     let phoneNumber = await append_233(res[i].phoneNumber);
-  //     await send(phoneNumber, req.body.message);
-  //   }
-  // }
-  // if (regionRecipient != null || regionRecipient != "") {
-  //   const res = await prisma.user.findMany({
-  //     where: { deleted: 0, regionId: regionRecipient },
-  //   });
-  //   for (let i = 0; i < res.length; i++) {
-  //     await send(res[i].phoneNumber, req.body.message);
-  //   }
-  // }
-  // if (districtRecipient != null || districtRecipient != "") {
-  //   const res = await prisma.user.findMany({
-  //     where: { deleted: 0, districtId: districtRecipient },
-  //   });
-  //   for (let i = 0; i < res.length; i++) {
-  //     console.log(res.phoneNumber);
-  //     await send(res[i].phoneNumber, req.body.message);
-  //   }
-  // }
+    for (let i = 0; i < res.length; i++) {
+      let phoneNumber = await append_233(res[i].phoneNumber);
+      await send(phoneNumber, req.body.message);
+    }
+  } else if (group == 2) {
+    //Send broadcast sms to region
 
+    const res = await prisma.user.findMany({
+      where: { deleted: 0, regionId: Number(recipientId) },
+    });
+
+    for (let i = 0; i < res.length; i++) {
+      let phoneNumber = await append_233(res[i].phoneNumber);
+      await send(phoneNumber, req.body.message);
+    }
+  }
+  
   res.status(200).json({ statusCode: 1, message: "Data saved" });
-  // } catch (error) {
-  //   console.log("Error: " + error);
-  //   if (error.code === "P2002")
-  //     return res
-  //       .status(400)
-  //       .json({ statusCode: 0, message: "dataVersion s should be unique" });
-  // }
+  } catch (error) {
+    console.log("Error: " + error);
+    if (error.code === "P2002")
+      return res
+        .status(400)
+        .json({ statusCode: 0, message: "dataVersion s should be unique" });
+  }
 };
 
 const get = async (req, res) => {
