7365605d09bbed39ab74116e3aade1da00d8a5dc
Kofi Agyemang <ebensakyi@gmail.com>
2023-03-27 11:01:37 +0000

    Update

--- pages/api/v1/inspection-data/premises-hazardous-waste-disposal.js
+++ pages/api/v1/inspection-data/premises-hazardous-waste-disposal.js
@@ -19,8 +19,11 @@ const post = async (req, res) => {
 
     const response = await prisma.premisesHazardousWasteDisposal.create({ data });
 
-    res.status(200).json({ statusCode: 1, message: "Data saved" });
-  } catch (error) {
+    if(response){
+      return  res.status(200).json({ statusCode: 1, message: "Data saved" });
+      }
+  
+      return  res.status(500).json({ statusCode: 0, message: "Data skipped" });  } catch (error) {
     // console.log("Error: " + error);
     // if (error.code === "P2002")
     //   return res
