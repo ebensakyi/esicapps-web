e9acbba57f752efd4c05dc375e35dc7ab8143168
Kofi Agyemang <ebensakyi@gmail.com>
2023-04-30 06:55:56 +0000

    Update

--- prisma/seed/region.ts
+++ prisma/seed/region.ts
@@ -1,80 +1,81 @@
 export const region = [
-  // {
-  //   name: "Ahafo",
-  //   abbrv: "AFR",
-  //   code: "01",
-  // },
+
   {
     name: "Greater Accra",
     abbrv: "GAR",
     code: "01",
   },
-  { name: "Ashanti", abbrv: "ASR", code: "02" },
-  //   {
-  //     name: "Bono",
-  //     abbrv: "BNR",
-  //     code: "03",
-  //   },
-  //   {
-  //     name: "Bono East",
-  //     abbrv: "BER",
-  //     code: "04",
-  //   },
-  //   {
-  //     name: "Central",
-  //     abbrv: "CNR",
-  //     code: "05",
-  //   },
-  //   {
-  //     name: "Eastern",
-  //     abbrv: "ETR",
-  //     code: "06",
-  //   },
+  { name: "Ashanti", abbrv: "ASR", code: "02" }, 
+   {
+    name: "Ahafo",
+    abbrv: "AFR",
+    code: "07",
+  },
+    {
+      name: "Bono",
+      abbrv: "BNR",
+      code: "03",
+    },
+    {
+      name: "Bono East",
+      abbrv: "BER",
+      code: "04",
+    },
+    {
+      name: "Central",
+      abbrv: "CNR",
+      code: "05",
+    },
+    {
+      name: "Eastern",
+      abbrv: "ETR",
+      code: "06",
+    },
 
-  //   {
-  //     name: "North East",
-  //     abbrv: "NER",
-  //     code: "08",
-  //   },
-  //   {
-  //     name: "Northern",
-  //     abbrv: "NTR",
-  //     code: "09",
-  //   },
-  //   {
-  //     name: "Oti",
-  //     abbrv: "OTR",
-  //     code: "10",
-  //   },
-  //  {
-  //     name: "Savannah",
-  //     abbrv: "SVR",
-  //     code: "11",
-  //   },
-  //   {
-  //     name: "Upper East",
-  //     abbrv: "UER",
-  //     code: "12",
-  //   },
-  //   {
-  //     name: "Upper West",
-  //     abbrv: "UWR",
-  //     code: "13",
-  //   },
+    {
+      name: "North East",
+      abbrv: "NER",
+      code: "08",
+    },
+    {
+      name: "Northern",
+      abbrv: "NTR",
+      code: "09",
+    },
+    {
+      name: "Oti",
+      abbrv: "OTR",
+      code: "10",
+    },
+   {
+      name: "Savannah",
+      abbrv: "SVR",
+      code: "11",
+    },
+    {
+      name: "Upper East",
+      abbrv: "UER",
+      code: "12",
+    },
+    {
+      name: "Upper West",
+      abbrv: "UWR",
+      code: "13",
+    },
 
-  //   {
-  //     name: "Volta",
-  //     abbrv: "VTR",
-  //     code: "14",
-  //   },
-  //   {
-  //     name: "Western",
-  //     abbrv: "WTR",
-  //     code: "15",
-  //   },
-  //   {
-  //     name: "Western North",
-  //     abbrv: "WNR",
-  //     code: "16",
-  //   },
+    {
+      name: "Volta",
+      abbrv: "VTR",
+      code: "14",
+    },
+    {
+      name: "Western",
+      abbrv: "WTR",
+      code: "15",
+    },
+    {
+      name: "Western North",
+      abbrv: "WNR",
+      code: "16",
+    },
 ];
