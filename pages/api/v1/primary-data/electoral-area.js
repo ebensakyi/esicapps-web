8885a497312f3445f7d9526fb897c33b75b23331
Kofi Agyemang <ebensakyi@gmail.com>
2023-05-15 21:31:16 +0000

    Update

--- pages/api/v1/primary-data/electoral-area.js
+++ pages/api/v1/primary-data/electoral-area.js
@@ -8,14 +8,15 @@ const post = async (req, res) => {
       districtId: req.body.data.districtId,
     };
     const electoralArea = await prisma.electoralArea.create({ data });
-    res
-      .status(200)
-      .json(electoralArea);
+    res.status(200).json(electoralArea);
   } catch (error) {
     if (error.code === "P2002")
       return res
         .status(200)
-        .json({ statusCode: 0, message: "electoralArea prefix should be unique" });
+        .json({
+          statusCode: 0,
+          message: "electoralArea prefix should be unique",
+        });
   }
 };
 
@@ -23,36 +24,31 @@ const get = async (req, res) => {
   try {
     let token = req?.cookies?.token || req?.query?.token;
 
-
-
     let data = await verifyToken(token);
     let districtId = data?.user?.districtId;
-
-  
-
 
     if (districtId) {
       const electoralArea = await prisma.electoralArea.findMany({
         where: { deleted: 0, districtId: Number(districtId) },
-        include: { District: true  },
+        include: { District: true },
       });
 
       return res.status(200).json(electoralArea);
     }
+    
     if (req?.query?.districtId) {
-
       const electoralArea = await prisma.electoralArea.findMany({
         where: { deleted: 0, districtId: Number(req.query.districtId) },
-      
       });
 
       return res.status(200).json(electoralArea);
     }
 
-
-    const electoralArea = await prisma.electoralArea.findMany({ where: { deleted: 0 }, include: { District: { include: { Region: true } } }, });
+    const electoralArea = await prisma.electoralArea.findMany({
+      where: { deleted: 0 },
+      include: { District: { include: { Region: true } } },
+    });
     return res.status(200).json(electoralArea);
-
   } catch (error) {
     console.log("Error: " + error);
   }
@@ -62,10 +58,10 @@ export default (req, res) => {
   req.method === "POST"
     ? post(req, res)
     : req.method === "PUT"
-      ? console.log("PUT")
-      : req.method === "DELETE"
-        ? console.log("DELETE")
-        : req.method === "GET"
-          ? get(req, res)
-          : res.status(404).send("");
+    ? console.log("PUT")
+    : req.method === "DELETE"
+    ? console.log("DELETE")
+    : req.method === "GET"
+    ? get(req, res)
+    : res.status(404).send("");
 };
