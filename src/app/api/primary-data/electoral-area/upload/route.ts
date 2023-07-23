f17ec07dbbb2e413d65a7a39c69f59b39687d581
Kofi Agyemang <ebensakyi@gmail.com>
2023-07-23 01:35:41 +0000

    Update

--- src/app/api/primary-data/electoral-area/upload/route.ts
+++ src/app/api/primary-data/electoral-area/upload/route.ts
@@ -7,107 +7,84 @@ import fs from "fs";
 import { nanoid } from "nanoid";
 import { writeFile } from "fs/promises";
 
-
 // export const config = {
 //     api: {
 //       bodyParser: false,
 //     },
 //   };
 export async function POST(request: Request) {
-    try {
-        const data = await request.formData()
+  try {
+    const data = await request.formData();
 
-        
+    const file: File | null = data.get("csvFile") as unknown as File;
+    const districtId = Number(data?.get("districtId"));
 
-        const file: File | null = data.get('csvFile') as unknown as File
-        const districtId = Number(data?.get('districtId'));
+    if (!file) {
+      return NextResponse.json({ success: false });
+    }
 
+    const bytes = await file.arrayBuffer();
+    const buffer = Buffer.from(bytes);
 
-        if (!file) {
-          return NextResponse.json({ success: false })
-        }
-      
-        const bytes = await file.arrayBuffer()
-        const buffer = Buffer.from(bytes)
+    let fileName = nanoid() + file.name;
 
-       // const fileName = nanoid() + ".csv";
+    const path = `./public/temp/${fileName}`;
 
-      
-        // With the file data in the buffer, you can do whatever you want with it.
-        // For this, we'll just write it to the filesystem in a new location
+    console.log("fileName ====>", fileName);
 
-        let fileName = file.name
-        const path = `./public/temp/${file.name}`
+    await writeFile(path, buffer);
 
+    let response = await readCSV(path, districtId);
 
-        
+   // await fs.unlinkSync(path);
 
-        console.log("fileName ====>",fileName);
-        
-        await writeFile(path, buffer);
-
-
-              let response = await readCSV(fileName, districtId);
-
-   
-  
-  
-   return NextResponse.json({});
+    return NextResponse.json({});
     // });
-    } catch (error: any) {
-      console.error(error);
-      return NextResponse.json(error);
-    }
+  } catch (error: any) {
+    console.error(error);
+    return NextResponse.json(error);
   }
+}
+
+const readCSV = async (filePath: any, districtId: any) => {
+  try {
+    let data: any = [];
 
 
-  const readCSV = async (fileName:any, districtId:any) => {
-    try {
-      let data:any = [];
-  //Upload file to temp folder
-    //   const csvFile = await file.csvFile;
-  
-    //   const csvData = fs.readFileSync(csvFile.filepath);
-       let filePath = `./public/temp/${fileName}`;
-  
-    //   let x = fs.writeFileSync(filePath, csvData);
-  
-console.log("filePathfilePath",filePath);
+    console.log("filePathfilePath", filePath);
 
+    //Read file
+    createReadStream(filePath)
+      .pipe(parse({ headers: true }))
+      .on("error", (error) => {
+        throw error.message;
+      })
+      .on("data", (row) => {
+        data.push(row);
+      })
+      .on("end", async () => {
+        let newData = await formatData(data, districtId);
 
-  
-  //Read file
-      createReadStream(filePath)
-        .pipe(parse({ headers: true }))
-        .on("error", (error) => {
-          throw error.message;
-        })
-        .on("data", (row) => {
-          data.push(row);
-        })
-        .on("end", async () => {
-          let newData = await formatData(data, districtId);
-  
-          await prisma.electoralArea.createMany({
-            data: newData,
-          });
+        await prisma.electoralArea.createMany({
+          data: newData,
         });
-        return data.length
-    } catch (error) {
-      console.log("csvUploader ==>", error);
-    }
-  };
-  
-  const formatData = async (data:any, districtId:any) => {
-    try {
-      let newData = data.map((row:any) => ({
-        districtId: Number(districtId),
-        name: row.name,
-      }));
-      console.log("newdata =>",newData);
-      
-      return newData;
-    } catch (error) {
-      console.log(error);
-    }
-  };
+      });
+    return data.length;
+  } catch (error) {
+    console.log("csvUploader ==>", error);
+  }
+};
+
+const formatData = async (data: any, districtId: any) => {
+  try {
+    let newData = data.map((row: any) => ({
+      districtId: Number(districtId),
+      name: row.name,
+    }));
+    console.log("newdata =>", newData);
+
+    return newData;
+  } catch (error) {
+    console.log(error);
+  }
+};
