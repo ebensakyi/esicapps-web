1172d6d67f7aab8204e1feda5d367c7de01fece1
Kofi Agyemang <ebensakyi@gmail.com>
2023-08-01 16:53:35 +0000

    Update

---
+++ src/components/submitted-data/DataView.tsx
@@ -0,0 +1,45 @@
+import React from 'react'
+
+export default function DataView() {
+  return (
+    <main id="main" className="main">
+    <div className="pagetitle">
+        <h1>Data </h1>
+        {/* <nav>
+            <ol className="breadcrumb">
+                <li className="breadcrumb-item">
+                    <a href="index.html">Home</a>
+                </li>
+                <li className="breadcrumb-item">Tables</li>
+                <li className="breadcrumb-item active">Data</li>
+            </ol>
+        </nav> */}
+    </div>
+    {/* End Page Title */}
+    <section className="section">
+        <div className="row">
+            <div className="col-lg-12">
+
+                {/* End Page Title */}
+                <div className="row">
+                    <div className="col-lg-12">
+                        <div className="card">
+                            <div className="card-header">
+                                
+                            </div>
+                            <div className="card-body table-responsive">
+                                {/* <h5 className="card-title">Datatables</h5> */}
+
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+
+            </div>
+        </div>
+    </section>
+</main>
+  )
+}
