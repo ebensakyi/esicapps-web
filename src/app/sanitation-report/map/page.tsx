72d32e4fc845310c53280be6377de3115d4316ac
Kofi Agyemang <ebensakyi@gmail.com>
2023-09-21 23:16:33 +0000

    update

--- src/app/sanitation-report/map/page.tsx
+++ src/app/sanitation-report/map/page.tsx
@@ -1,38 +1,18 @@
 export const dynamic = "force-dynamic";
 
 import { SERVER_BASE_URL } from "@/config";
+import Map from "@/src/components/map/Map";
+//import dynamic from "next/dynamic";
 
- import Notification from "@/src/components/messaging/Notification";
-// import dynamic from "next/dynamic";
-
-// const Notification = dynamic(() => import('@/src/components/messaging/Notification'), {
-//     ssr: false, // Prevent pre-rendering at build time
-//   });
-async function getNotifications() {
-
-    let response = await fetch(`${SERVER_BASE_URL}/api/messaging/notification`, { cache: 'no-store' });
-
-    if (!response.ok) {
-        throw new Error('Failed to fetch data')
-    }
-    return await response.json();
-
-}
+// const Map = dynamic(() => import("@/src/components/map/Map"), {
+//     ssr: false
+// });
 
 
-async function getSendingType() {
+async function getData(searchParams: any) {
+    let { formId } = searchParams
 
-    let response = await fetch(`${SERVER_BASE_URL}/api/primary-data/sending-type`, { cache: 'no-store' });
-
-    if (!response.ok) {
-        throw new Error('Failed to fetch data')
-    }
-    return await response.json();
-
-}
-async function getUsers() {
-
-    let response = await fetch(`${SERVER_BASE_URL}/api/user`, { cache: 'no-store' });
+    let response = await fetch(`${SERVER_BASE_URL}/api/map?formId=${formId}`, { next: { revalidate: 14400 } });
 
     if (!response.ok) {
         throw new Error('Failed to fetch data')
@@ -41,46 +21,19 @@ async function getUsers() {
 
 }
 
-async function getRegions() {
-
-    let response = await fetch(`${SERVER_BASE_URL}/api/primary-data/region`, { cache: 'no-store' });
-
-    if (!response.ok) {
-        throw new Error('Failed to fetch data')
-    }
-    return await response.json();
-
-}
-
-async function getDistricts() {
-
-    let response = await fetch(`${SERVER_BASE_URL}/api/primary-data/district`, { cache: 'no-store' });
-
-    if (!response.ok) {
-        throw new Error('Failed to fetch data')
-    }
-    return await response.json();
-
-}
-
-export default async function Page() {
+export default async function Page({ searchParams }: any) {
 
 
-    const notifications = await getNotifications()
-    const sendingTypes = await getSendingType()
-    const regions = await getRegions()
-    const districts = await getDistricts()
-    const users = await getUsers()
+    const mapData = await getData(searchParams)
 
-    let data = {
-        notifications, sendingTypes, regions, districts, users, fallback: false,
-    }
+
+    let data = { mapData }
 
 
 
 
 
-    return <Notification data={data} />
+    return <Map data={data}/>
 
 
 }
