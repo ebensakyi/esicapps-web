cf2e2d0c13f2bec85afb0367b553ea88554d1317
Kofi Agyemang <ebensakyi@gmail.com>
2023-10-11 12:15:30 +0000

    update

--- src/app/api/sanitation-report/district/route.ts
+++ src/app/api/sanitation-report/district/route.ts
@@ -2,18 +2,12 @@ export const dynamic = "force-dynamic";
 
 import { NextResponse } from "next/server";
 import { prisma } from "@/prisma/db";
-import { logActivity } from "@/utils/log";
-
-
-
 
 
 export async function GET(request: Request) {
   try {
     const { searchParams } = new URL(request.url);
    
-
-    
         const response = await prisma.district.findMany({
           where: { deleted: 0 },
           orderBy: {
--- src/app/user/guide/page.tsx
+++ src/app/user/guide/page.tsx
@@ -2,17 +2,10 @@ export const dynamic = "force-dynamic";
 
 import { LOGIN_URL, SERVER_BASE_URL } from "@/config";
 
-import { Suspense } from "react";
 import Guide from "../../../components/user/Guide";
 async function getUserGuides() {
 
-
-console.log("${SERVER_BASE_URL}/api/user/guide${SERVER_BASE_URL}/api/user/guide ",`${SERVER_BASE_URL}/api/user/guide`);
-
     let response = await fetch(`${SERVER_BASE_URL}/api/user/guide`, { cache: 'no-store' });
-
-    console.log("getUserGuides===> ", response.ok);
-
 
     if (response.status != 200) {
         throw new Error('Failed to fetch data')
