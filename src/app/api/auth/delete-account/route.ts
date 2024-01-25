afb5e0ef5dc4cca3c538e026568225a34629f588
Kofi Agyemang <ebensakyi@gmail.com>
2024-01-25 23:33:50 +0000

    -

--- src/app/api/auth/delete-account/route.ts
+++ src/app/api/auth/delete-account/route.ts
@@ -1,15 +1,14 @@
 import { NextResponse } from "next/server";
 import { prisma } from "@/prisma/db";
+import bcrypt from "bcryptjs";
 
 import { v4 as uuidv4 } from "uuid";
 
 export async function POST(request: Request) {
   try {
-  const res = await request.json();
+    const res = await request.json();
 
-  let phoneNumber = res.phoneNumber;
-
-    
+    let phoneNumber = res.phoneNumber;
 
     const user: any = await prisma.user.findFirst({
       where: {
@@ -18,8 +17,6 @@ export async function POST(request: Request) {
       },
     });
 
-    
-   
     if (!user) {
       return NextResponse.json(
         {
@@ -36,16 +33,62 @@ export async function POST(request: Request) {
       },
       data: {
         deleted: 1,
-        phoneNumber: phoneNumber+"+deleted-" + uuidv4()
+        phoneNumber: phoneNumber + "+deleted-" + uuidv4(),
       },
     });
 
-
-
     return NextResponse.json({});
-
   } catch (error: any) {
     console.log("ERR==> Error: " + error.message);
     return NextResponse.json({ message: error.message });
+  }
+}
+
+export async function DELETE(
+  request: Request
+) {
+  try {
+    const res = await request.json();
+
+    console.log(res);
+    
+    const password = res.password;
+    const phoneNumber = res.phoneNumber;
+
+    const user: any = await prisma.user.findFirst({
+      where: {
+        phoneNumber: phoneNumber,
+
+        deleted: 0,
+      },
+    });
+
+    if (!user) {
+      return NextResponse.json(
+        {
+          message: "User Account not found.",
+        },
+        { status: 201 }
+      );
+    }
+    let isValid = await bcrypt.compare(password, user.password);
+
+    if (isValid) {
+      await prisma.user.update({
+        where: {
+          id: user.id,
+          deleted: 0,
+        },
+        data: {
+          deleted: 1,
+          phoneNumber: phoneNumber + "+deleted-" + uuidv4(),
+        },
+      });
+    }
+
+    return NextResponse.json({});
+  } catch (error) {
+    console.log("error===>", error);
+    return NextResponse.json(error);
   }
 }
