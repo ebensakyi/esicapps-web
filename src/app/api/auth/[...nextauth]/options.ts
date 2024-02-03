fcdc1fe48b685ff401e8e7bf3b485fdc327f3840
Kofi Agyemang <ebensakyi@gmail.com>
2024-02-03 23:52:42 +0000

    -

--- src/app/api/auth/[...nextauth]/options.ts
+++ src/app/api/auth/[...nextauth]/options.ts
@@ -49,12 +49,12 @@ export const authOptions: NextAuthOptions = {
   },
   callbacks: {
 		async jwt({ token, user }) {
-      console.log({ ...token, ...user});
+      console.log("jwt===>",{ ...token, ...user});
       
 			return { ...token, ...user };
 		},
 		async session({ session, token }: { session: Session; token: any }) {
-      console.log({ ...session, user: token });
+      console.log("session===>",{ ...session, user: token });
       
 			return { ...session, user: token };
 		},
