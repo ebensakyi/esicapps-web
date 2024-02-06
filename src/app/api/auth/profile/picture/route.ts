9b1249a80a228751a8ca5704269711b7cd131e67
Kofi Agyemang <ebensakyi@gmail.com>
2024-02-06 06:10:48 +0000

    -

--- src/app/api/auth/profile/picture/route.ts
+++ src/app/api/auth/profile/picture/route.ts
@@ -1,7 +1,6 @@
 import { NextResponse } from "next/server";
 import { prisma } from "@/prisma/db";
-import { logActivity } from "@/utils/log";
-import { generateCode } from "@/utils/generate-code";
+
 
 import bcrypt from "bcryptjs";
 import { getServerSession } from "next-auth";
@@ -12,8 +11,6 @@ export async function POST(request: Request) {
   try {
     const data = await request.formData();
 
-   
-
     const file: File | null = data.get("imageFile") as unknown as File;
     const userId = Number(data?.get("userId"));
 
@@ -23,12 +20,24 @@ export async function POST(request: Request) {
       const data = {
         imagePath: fileName,
       };
-      const ui = await prisma.user.update({
-        data,
-        where: { id: Number(userId) },
+      const user = await prisma.userImage.findFirst({
+        where: { userId: Number(userId) },
       });
+      if (user) {
+   await prisma.userImage.update({
+          data,
+          where: { id: Number(userId) },
+        });
+      } else {
+       await prisma.userImage.create({
+          data:{
+            imagePath: fileName,
+            userId: userId
+          }
+        });
+      }
 
-      await upload2S3(fileName,"esicapps-profile-images");
+      await upload2S3(fileName, "esicapps-profile-images");
 
       return NextResponse.json({ data: fileName }, { status: 200 });
     }
