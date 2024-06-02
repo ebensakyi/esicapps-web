3f24af6b8eed16712fecc407fefd61a4e1ae817a
Kofi Agyemang <ebensakyi@gmail.com>
2024-06-02 22:01:17 +0000

    -

--- src/app/api/premises-inspection/inspection-pictures/route.ts
+++ src/app/api/premises-inspection/inspection-pictures/route.ts
@@ -1,41 +1,37 @@
 import { NextResponse } from "next/server";
 import { prisma } from "@/prisma/db";
 import { logActivity } from "@/utils/log";
-import { upload2S3, saveFileOnDisk } from "@/utils/upload";
+import { uploadBase64Image } from "@/utils/upload-base64";
+import { AWS_S3_ESICAPPS_IMAGES } from "@/config";
 import { logger } from "@/logger";
+// import { AWS_S3_ESICAPPS_IMAGES } from "@/config";
 
 export async function POST(request: Request) {
   try {
 
-
-
-    const data = await request.formData();
-
-    const file: File | null = data.get("imageFile") as unknown as File;
-    const inspectionId = data?.get("inspectionId");
-
-    const formSectionImageId = data?.get("formSectionImageId");
-
+    const res = await request.json();
     
 
-    let fileName = await saveFileOnDisk(file);
+    const inspectionId = res?.inspectionId;
+
+    const formSectionImageId = res.formSectionImageId;
+    let encodedImage: string = res.encodedImage;
 
 
-    if (fileName != "0") {
-      const data = {
-        inspectionId: inspectionId,
-        imagePath: fileName,
-        formSectionImageId:
-          formSectionImageId == "null" ? 1 : Number(formSectionImageId),
-      };
 
-      const ip = await prisma.inspectionPictures.create({ data } as any);
-      await upload2S3(fileName,"esicapps-images");
+    let fileName = await uploadBase64Image(encodedImage,AWS_S3_ESICAPPS_IMAGES);
 
-      return NextResponse.json({ data: fileName }, { status: 200 });
-    }
+    const data = {
+      inspectionId: inspectionId,
+      imagePath: fileName,
+      formSectionImageId:
+        formSectionImageId == "null" ? 1 : Number(formSectionImageId),
+    };
 
-    return NextResponse.json({ message: "An error occurred" }, { status: 500 });
+    const ip = await prisma.inspectionPictures.create({ data } as any);
+
+    return NextResponse.json({ data: fileName }, { status: 200 });
+
   } catch (error) {
     console.log(error);
     logger.error("INSPECTION_PICTURES==>",error);
