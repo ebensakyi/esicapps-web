47990e39462d651636ff154c0f6bfd7d729b2346
Kofi Agyemang <ebensakyi@gmail.com>
2024-06-02 17:44:06 +0000

    -

--- src/app/api/premises-inspection/water-section/route.ts
+++ src/app/api/premises-inspection/water-section/route.ts
@@ -2,6 +2,7 @@ import { NextResponse } from "next/server";
 import { prisma } from "@/prisma/db";
 import { logActivity } from "@/utils/log";
 import { logger } from "@/logger";
+import { convertStringToArray } from "@/utils/array-converter";
 
 
 export async function POST(request: Request) {
@@ -45,11 +46,27 @@ export async function GET(request: Request) {
   try {
     const { searchParams } = new URL(request.url);
     const userId = Number(searchParams.get("userId"));
+    const inspectionIds = searchParams.get("inspectionIds");
 
-    if (!userId) return NextResponse.json({});
+    const array = convertStringToArray(inspectionIds);
+
+    if (!userId) {
+      return NextResponse.json({ status: 200 });
+    }
+
+    const whereClause: {
+      userId: number;
+      deleted: number;
+      inspectionId?: { in: string[] };
+    } = { userId: userId, deleted: 0 };
+
+    if (array.length > 0) {
+      whereClause.inspectionId = { in: array };
+    }
+
 
     const response = await prisma.waterSection.findMany({
-      where: { userId: userId, deleted: 0 },
+      where: whereClause,
     });
 
     return NextResponse.json(response, {
