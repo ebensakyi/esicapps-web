bd51b809c7616cb271162d1da62a522ada18bea9
Kofi Agyemang <ebensakyi@gmail.com>
2025-01-16 10:44:51 +0000

    _

--- config/index.ts
+++ config/index.ts
@@ -5,7 +5,7 @@ const dev = process.env.NODE_ENV !== 'production';
 
 export const SERVER_BASE_URL = dev ? 'http://localhost:3000' : 'https://dashboard.esicapps.org'
 export const LOGIN_URL = dev ? "/auth/login" : "https://dashboard.esicapps.org/auth/login"
-export const _S3_URL ="https://esicapps-images.s3.eu-west-2.amazon.com/" 
+export const AWS_S3_URL ="https://esicapps-images.s3.eu-west-2.amazon.com/" 
 
 export const AWS_S3_PROFILE_IMAGES_URL ="https://esicapps-profile-images.s3.eu-west-2.amazonaws.com/"
 
